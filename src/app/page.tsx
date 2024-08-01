a45300876239627c99198ead58173150b37b9798
karanMathur89 <74524108+karanMathur89@users.noreply.github.com>
2024-08-01 12:06:45 +0530

    feat: added onClick

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,20 @@ export default function Home() {
   //* STATES
   const [selectedColor, setSelectedColor] = useState(colorVariants[0])
   const [selectedSize, setSelectedSize] = useState(sizeVariants[2])
+
   return (
     <main>
+      <h1 className="mb-48 text-3xl font-bold">With useState</h1>
       <section className="rounded bg-gray-100 p-4">
-        <h1 className="mb-1 text-2xl font-bold">Snug T-shirt</h1>
+        <h2 className="mb-1 text-2xl font-bold">Snug T-shirt</h2>
         <span className="text inline-block rounded-full bg-blue-600 px-2 py-1 text-sm font-medium text-blue-50">
           $20.00
         </span>
         <div className="mt-6 grid grid-cols-6 gap-4">
-          <div className="col-start-1 col-end-3 grid place-content-center rounded border border-dashed border-gray-400 bg-white">
+          <div className="col-start-1 col-end-3 grid w-full items-center rounded border border-dashed border-gray-400 bg-white">
             <div
               className={cn([
-                "h-12 w-12 rounded outline outline-offset-2",
+                "mx-auto aspect-square w-1/2 rounded outline outline-offset-4",
                 { "bg-black": selectedColor === "black" },
                 { "bg-white": selectedColor === "white" },
                 { "bg-blue-600 outline-blue-600": selectedColor === "blue" },
@@ -30,11 +32,12 @@ export default function Home() {
           </div>
           <div className="col-start-3 -col-end-1 space-y-6">
             <section className="space-y-2">
-              <h2 className="font-medium uppercase tracking-tight">Colour</h2>
+              <h3 className="font-medium uppercase tracking-tight">Colour</h3>
               <ul className="flex flex-wrap gap-2">
                 {colorVariants.map((color) => (
                   <li key={color}>
                     <button
+                      onClick={() => setSelectedColor(color)}
                       className={cn([
                         "rounded-full border-2 border-gray-300 bg-gray-50 px-3 py-1 capitalize hover:bg-white",
                         { "border-blue-600": color === selectedColor },
@@ -47,11 +50,12 @@ export default function Home() {
               </ul>
             </section>
             <section className="space-y-2">
-              <h2 className="font-medium uppercase tracking-tight">Sizes</h2>
+              <h3 className="font-medium uppercase tracking-tight">Sizes</h3>
               <ul className="flex flex-wrap gap-2">
                 {sizeVariants.map((size) => (
                   <li key={size}>
                     <button
+                      onClick={() => setSelectedSize(size)}
                       className={cn([
                         "min-w-[5ch] rounded-full border-2 border-gray-300 bg-gray-50 px-3 py-1 uppercase hover:bg-white",
                         { "border-blue-600": size === selectedSize },
